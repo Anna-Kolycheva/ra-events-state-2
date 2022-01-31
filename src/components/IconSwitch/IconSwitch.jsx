de655a08875aef68ddd1d91ab6ec7efca2de120e
Kolycheva Anna <annakolyt@gmail.com>
2022-01-31 16:20:53 +0700

    added PropTypes

--- src/components/IconSwitch/IconSwitch.jsx
+++ src/components/IconSwitch/IconSwitch.jsx
@@ -1,13 +1,20 @@
-import './IconSwitch.css'
-import React from 'react'
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+
+import './IconSwitch.css';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 export default function IconSwitch(props) {
+   const { icon, onSwitch } = props;
    return (
-     <div 
-         className='iconswitch material-icons'
-         onClick={() => props.onSwitch(props.icon)}
-     >
-         {props.icon}
-     </div>
-   )
+      <div className="iconswitch material-icons" onClick={() => onSwitch(icon)}>
+         {icon}
+      </div>
+   );
 }
+
+IconSwitch.propTypes = {
+   icon: PropTypes.string.isRequired,
+   onSwitch: PropTypes.func.isRequired,
+};
--- src/components/ListView/ListView.jsx
+++ src/components/ListView/ListView.jsx
@@ -1,27 +1,31 @@
-import './ListView.css'
-import React from 'react'
+import './ListView.css';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function ListView(props) {
+export default function ListView({ products }) {
    return (
-     <div className='listview'>
-         {props.products.map((product) => {
-            return (
-               <div 
-               className='listview-item'
-               key={product.name + product.color}
-               >
-                  <img 
-                    className='listview-item-img'
-                    src={product.img} 
-                    alt={product.name}
-                  /> 
-                  <div className='listview-item-name'>{product.name.toUpperCase()}</div>
-                  <div className='listview-item-color'>{product.color}</div>
-                  <div className='listview-item-price'>${product.price}</div>
-                  <button className='listview-item-button'>add to cart</button>
+      <div className="listview">
+         {products.map((product) => (
+            <div className="listview-item" key={product.name + product.color}>
+               <img
+                  className="listview-item-img"
+                  src={product.img}
+                  alt={product.name}
+               />
+               <div className="listview-item-name">
+                  {product.name.toUpperCase()}
                </div>
-            )
-         })}
-     </div>
-   )
+               <div className="listview-item-color">{product.color}</div>
+               <div className="listview-item-price">${product.price}</div>
+               <button type="button" className="listview-item-button">
+                  add to cart
+               </button>
+            </div>
+         ))}
+      </div>
+   );
 }
+
+ListView.propTypes = {
+   products: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
